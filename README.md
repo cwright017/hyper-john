3ab34e20d477efde49e6e9e515b97901e6a5fa74
Carl Wright <C.wright017@gmail.com>
2016-11-09 19:40:26 +0000

    And his name is JOHN CENA! :boom:

---
+++ README.md
@@ -0,0 +1,21 @@
+# Hyper: John!
+
+A 10% change of getting hit with John Cena when opening new [Hyper](https://hyperterm.org) instances (windows/tabs/splits).
+
+### Install
+
+1. Open Hyper's preferences with <kbd>Cmd</kbd> + <kbd>,</kbd> (or open `~/.hyper.js` in an editor).
+2. Add `hyper-john` to your list of plugins:
+
+  ```javascript
+  plugins: [
+    'hyper-john'
+  ],
+  ```
+3. Fully reload Hyper (`Cmd + Shift + R`), and :boom:!
+
+### License
+
+MIT © [CWright017][author]
+
+[author]:     https://github.com/Cwright017
---
+++ index.js
@@ -0,0 +1,27 @@
+const AUDIO_SRC = require('./cena.js');
+
+module.exports.decorateTerm = (Term, {React, notify}) => {
+  return class extends React.Component {
+    constructor (props, context) {
+      super(props, context)
+      this._onTerminal = this._onTerminal.bind(this);
+      this._audio = new Audio(AUDIO_SRC);
+    }
+
+    _onTerminal (term) {
+      if (Math.floor(Math.random() * 10) === 1) {
+        this._audio.play();
+      }
+
+      if (this.props.onTerminal) {
+        this.props.onTerminal(term);
+      }
+    }
+
+    render () {
+      return React.createElement(Term, Object.assign({}, this.props, {
+        onTerminal: this._onTerminal
+      }))
+    }
+  }
+}
